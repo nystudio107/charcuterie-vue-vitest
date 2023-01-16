90e2cfaea5ef1cb01ca16819af4c761d1f69020b
Andrew Welch <andrew@nystudio107.com>
2023-01-16 16:16:06 -0500

    refactor: Clean up CSS

--- src/vue/components/CharcuterieBoardLogo.vue
+++ src/vue/components/CharcuterieBoardLogo.vue
@@ -15,7 +15,8 @@
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
-  filter: drop-shadow(0 0 2em rgba(255, 193, 100, 0.67));
+  filter: drop-shadow(0 0 2em rgba(255 193 100 / 67%));
 }
 </style>
