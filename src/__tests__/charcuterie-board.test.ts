f5c6b36d1afe2e8e22b3ba25d87d1280f7a6c900
Andrew Welch <andrew@nystudio107.com>
2023-01-17 14:25:39 -0500

    refactor: Updated tests

--- src/__tests__/charcuterie-board.test.ts
+++ src/__tests__/charcuterie-board.test.ts
@@ -1,21 +1,24 @@
 import {CharcuterieItemType} from "../@types/charcuterie-enums";
-import {getAllItems, getItemsOfType, addItem} from '../js/charcuterie-board';
+import {clearAllItems, getAllItems, getItemsOfType, addItem} from '../js/charcuterie-board';
 import {getInventoryItem} from "../js/charcuterie-inventory";
 import {it, expect, describe} from 'vitest';
 
 describe("charcuterie-board.ts", () => {
   it('should add items to the charcuterie board', () => {
+    clearAllItems();
     addItem(getInventoryItem('olives'));
     addItem(getInventoryItem('salami'));
     expect(getAllItems()).toMatchSnapshot();
   });
 
   it('should handle adding undefined items', () => {
+    clearAllItems();
     addItem(undefined);
     expect(getAllItems()).toMatchSnapshot();
   });
 
   it ('should return the proper types of items', () => {
+    clearAllItems();
     addItem(getInventoryItem('olives'));
     addItem(getInventoryItem('salami'));
     const fruits = getItemsOfType(CharcuterieItemType.Fruit);
