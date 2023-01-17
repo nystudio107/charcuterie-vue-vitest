48c09f2210e1f193ef3d88f40897801a164fcbdb
Andrew Welch <andrew@nystudio107.com>
2023-01-17 14:27:49 -0500

    refactor: Add clearAllItems()

--- src/js/charcuterie-board.ts
+++ src/js/charcuterie-board.ts
@@ -6,14 +6,14 @@ export function getAllItems() {
   return charcuterieBoard;
 }
 
-export function clearBoard() {
-  charcuterieBoard = [];
+export function clearAllItems() {
+  charcuterieBoard.length = 0;
 }
 export function getItemsOfType(type: CharcuterieItemType): CharcuterieItem[] {
   return charcuterieBoard.filter((value: CharcuterieItem) => value.type === type)
 }
 
-export function addCharcuterieItem(item: CharcuterieItem|undefined) {
+export function addItem(item: CharcuterieItem|undefined) {
   if (typeof item !== 'undefined') {
     charcuterieBoard.push(item);
   }
