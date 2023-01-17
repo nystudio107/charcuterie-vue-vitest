c78d241fb53ef171f3892854d7bc45e75a1807a7
Andrew Welch <andrew@nystudio107.com>
2023-01-17 15:55:05 -0500

    refactor: Add more App tests

--- src/__tests__/App.test.ts
+++ src/__tests__/App.test.ts
@@ -3,12 +3,49 @@ import App from "../vue/App.vue";
 import {describe, it, expect} from "vitest";
 
 describe("App.vue", () => {
+  it("should have <div> element with the 'row' class", () => {
+    const wrapper = mount(App, {
+    });
+    const columns = wrapper.findAll('div.row');
+    expect(columns.length).toBe(1);
+  });
 
-  it("should have the following components: <CharcuterieBoardLogo>, ", () => {
+  it("should have <div> elements with the 'column' class", () => {
+    const wrapper = mount(App, {
+    });
+    const columns = wrapper.findAll('div.column');
+    expect(columns.length).toBe(2);
+  });
+
+  it("the first column should have a <h1> with the title 'Charcuterie Board'", () => {
+    const wrapper = mount(App, {
+    });
+    const columns = wrapper.findAll('div.column');
+    const header = columns[0].find('h1');
+    expect(header.text()).toBe('Charcuterie Board');
+  });
+
+  it("the second column should have a <h1> with the title 'Inventory'", () => {
+    const wrapper = mount(App, {
+    });
+    const columns = wrapper.findAll('div.column');
+    const header = columns[1].find('h1');
+    expect(header.text()).toBe('Inventory');
+  });
+
+  it("should have the following components: <CharcuterieBoardLogo>, <CharcuterieBoard> & <CharcuterieInventory>", () => {
     const wrapper = mount(App, {
       shallow: true
     });
-    const component = wrapper.find('charcuterie-board-logo-stub');
-    expect(component.exists()).toBe(true);
+    const components = ['CharcuterieBoardLogo', 'CharcuterieBoard', 'CharcuterieInventory'];
+    components.forEach((name) => {
+      const component = wrapper.find(kebabCase(name) + '-stub');
+      expect(component.exists()).toBe(true);
+    });
   });
 });
+
+const kebabCase = string => string
+  .replace(/([a-z])([A-Z])/g, "$1-$2")
+  .replace(/[\s_]+/g, '-')
+  .toLowerCase();
--- src/vue/App.vue
+++ src/vue/App.vue
@@ -1,9 +1,45 @@
 <script setup lang="ts">
 import CharcuterieBoardLogo from './components/CharcuterieBoardLogo.vue'
+import CharcuterieBoard from './components/CharcuterieBoard.vue'
+import CharcuterieInventory from './components/CharcuterieInventory.vue'
 </script>
 
 <template>
   <div>
     <CharcuterieBoardLogo />
+    <div class="row">
+      <div class="column">
+        <h1>Charcuterie Board</h1>
+        <CharcuterieBoard />
+      </div>
+      <div class="column">
+        <h1>Inventory</h1>
+        <CharcuterieInventory />
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+h1 {
+  font-size: 2.2em;
+  line-height: 1.1;
+  color: rgba(97 65 16 / 100%);
+}
+
+.row {
+  display: flex;
+  flex-flow: row;
+  margin-top: -1px;
+  margin-left: -1px;
+}
+
+.column {
+  display: flex;
+  flex-flow: column;
+  flex: 1;
+  outline: 1px solid rgba(97 65 16 / 100%);
+  margin-top: 1px;
+  margin-left: 1px;
+}
+</style>
