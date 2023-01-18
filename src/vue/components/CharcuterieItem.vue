e46a47d838583ed5893718f3b6224a27fa61c1da
Andrew Welch <andrew@nystudio107.com>
2023-01-18 18:26:17 -0500

    refactor: Cross out veggies

--- src/vue/components/CharcuterieItem.vue
+++ src/vue/components/CharcuterieItem.vue
@@ -6,11 +6,12 @@ defineProps<{
 
 <template>
   <div class="box">
-    <div class="wrapper">
+    <div :class="['wrapper', item.type]">
       <img
         :src="`../../src/assets/${item.type}.svg`"
         class="icon"
         :alt="`${item.name}`"
+        :title="`${item.type}`"
       >
       <span class="name">{{ item.name }}</span>
     </div>
@@ -25,7 +26,7 @@ defineProps<{
 
 .wrapper {
   padding: 0.5em;
-  width: 150px;
+  width: 240px;
   display: flex;
   justify-content: left;
 }
@@ -40,5 +41,10 @@ defineProps<{
   height: 1.5em;
   margin-right: 0.5em;
   will-change: filter;
+}
+
+.vegetable {
+  background: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' preserveAspectRatio='none' viewBox='0 0 100 100'><path d='M100 0 L0 100 ' stroke='red' stroke-width='2'/><path d='M0 0 L100 100 ' stroke='red' stroke-width='2'/></svg>") no-repeat center center;
+  background-size: 100% 100%, auto;
 }
 </style>
