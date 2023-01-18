3fd8ffd5bdefe19d984fc50064d829e32500949f
Andrew Welch <andrew@nystudio107.com>
2023-01-18 18:26:04 -0500

    refactor: Flesh out inventory

--- src/js/charcuterie-inventory.ts
+++ src/js/charcuterie-inventory.ts
@@ -2,15 +2,145 @@ import {CharcuterieItemType} from '@/@types/charcuterie-enums';
 
 const charcuterieInventory: CharcuterieItem[]  = [
   {
+    name: 'graham',
+    type: CharcuterieItemType.Cracker
+  },
+  {
+    name: 'cream',
+    type: CharcuterieItemType.Cracker
+  },
+  {
+    name: 'matzo',
+    type: CharcuterieItemType.Cracker
+  },
+  {
+    name: 'graham',
+    type: CharcuterieItemType.Cracker
+  },
+  {
+    name: 'oatcake',
+    type: CharcuterieItemType.Cracker,
+  },
+  {
+    name: 'rice',
+    type: CharcuterieItemType.Cracker
+  },
+  {
     name: 'salami',
     type: CharcuterieItemType.Meat,
-    calories: 200,
+  },
+  {
+    name: 'soppressata',
+    type: CharcuterieItemType.Meat,
+  },
+  {
+    name: 'calabrese',
+    type: CharcuterieItemType.Meat,
+  },
+  {
+    name: 'mortadella',
+    type: CharcuterieItemType.Meat,
+  },
+  {
+    name: 'prosciutto',
+    type: CharcuterieItemType.Meat,
+  },
+  {
+    name: 'chorizo',
+    type: CharcuterieItemType.Meat,
+  },
+  {
+    name: 'capicola',
+    type: CharcuterieItemType.Meat,
+  },
+  {
+    name: 'cheddar',
+    type: CharcuterieItemType.Cheese,
+  },
+  {
+    name: 'roquefort',
+    type: CharcuterieItemType.Cheese,
+  },
+  {
+    name: 'swiss',
+    type: CharcuterieItemType.Cheese,
+  },
+  {
+    name: 'pecorino-romano',
+    type: CharcuterieItemType.Cheese,
+  },
+  {
+    name: 'parmigiano-reggiano',
+    type: CharcuterieItemType.Cheese,
+  },
+  {
+    name: 'gruyere',
+    type: CharcuterieItemType.Cheese,
+  },
+  {
+    name: 'provolone',
+    type: CharcuterieItemType.Cheese,
+  },
+  {
+    name: 'peanuts',
+    type: CharcuterieItemType.Nut,
+  },
+  {
+    name: 'almond',
+    type: CharcuterieItemType.Nut,
+  },
+  {
+    name: 'cashew',
+    type: CharcuterieItemType.Nut,
+  },
+  {
+    name: 'walnut',
+    type: CharcuterieItemType.Nut,
+  },
+  {
+    name: 'brazil',
+    type: CharcuterieItemType.Nut,
+  },
+  {
+    name: 'pistachio',
+    type: CharcuterieItemType.Nut,
+  },
+  {
+    name: 'peanuts',
+    type: CharcuterieItemType.Nut,
+  },
+  {
+    name: 'apple',
+    type: CharcuterieItemType.Fruit,
   },
   {
     name: 'olives',
     type: CharcuterieItemType.Fruit,
-    calories: 100
-  }
+  },
+  {
+    name: 'strawberry',
+    type: CharcuterieItemType.Fruit,
+  },
+  {
+    name: 'cherry',
+    type: CharcuterieItemType.Fruit,
+  },
+  {
+    name: 'blackberry',
+    type: CharcuterieItemType.Fruit,
+  },
+  {
+    name: 'raspberry',
+    type: CharcuterieItemType.Fruit,
+  },
+  {
+    name: 'pear',
+    type: CharcuterieItemType.Fruit,
+  },
+  {
+    name: 'carrot',
+    type: CharcuterieItemType.Vegetable,
+  },
 ];
 
 export function getInventory() {
