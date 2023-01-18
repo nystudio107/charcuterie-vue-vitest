7eed7358b80ddef78cdc3ab2a58851311d96e218
Andrew Welch <andrew@nystudio107.com>
2023-01-18 10:03:02 -0500

    refactor: Add CharcuterieItem and images

--- src/vue/components/CharcuterieInventory.vue
+++ src/vue/components/CharcuterieInventory.vue
@@ -1,8 +1,20 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import {getAllItems } from '@/js/charcuterie-inventory';
+import CharcuterieItem from './CharcuterieItem.vue';
+
+const items = ref(getAllItems());
 </script>
 
 <template>
   <div>
+    <CharcuterieItem
+      v-for="item in items"
+      :key="item.name"
+      :item="item"
+    >
+      {{ item.name }}
+    </CharcuterieItem>
   </div>
 </template>
 
---
+++ src/vue/components/CharcuterieItem.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+
+defineProps<{
+  item: CharcuterieItem,
+}>();
+</script>
+
+<template>
+  <div class="box">
+    <div class="wrapper">
+      <img
+        :src="`../../src/assets/${item.type}.svg`"
+        class="icon"
+        :alt="`${item.name}`"
+      >
+      <span class="name">{{ item.name }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.box {
+  display: flex;
+  justify-content: center;
+}
+
+.wrapper {
+  padding: 0.5em;
+  width: 150px;
+  display: flex;
+  justify-content: left;
+}
+
+.name {
+  font-size: 1.7em;
+  color: rgba(97 65 16 / 80%);
+}
+
+.icon {
+  text-align: center;
+  height: 2em;
+  margin-right: 0.5em;
+  will-change: filter;
+}
+</style>
