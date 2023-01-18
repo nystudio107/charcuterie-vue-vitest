d8314be8141ab6fda47dc72b9bd59ee2c15657d2
Andrew Welch <andrew@nystudio107.com>
2023-01-18 10:02:45 -0500

    refactor: Add @ alias

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslint from 'vite-plugin-eslint'
 import stylelint from 'vite-plugin-stylelint';
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,11 @@ export default defineConfig({
     origin: 'http://localhost:3000',
     port: 3000,
     strictPort: true,
+  },
+  resolve: {
+    alias: [
+      {find: '@', replacement: path.resolve(__dirname, './src')},
+    ],
   },
   test: {
     globals: true,
