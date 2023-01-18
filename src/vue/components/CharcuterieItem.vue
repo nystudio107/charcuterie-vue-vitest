3d40eeb80189f012e3ed2c212542ed82e3368efc
Andrew Welch <andrew@nystudio107.com>
2023-01-18 11:12:48 -0500

    refactor: Smaller size

--- src/vue/components/CharcuterieItem.vue
+++ src/vue/components/CharcuterieItem.vue
@@ -32,13 +32,13 @@ defineProps<{
 }
 
 .name {
-  font-size: 1.7em;
+  font-size: 1.3em;
   color: rgba(97 65 16 / 80%);
 }
 
 .icon {
   text-align: center;
-  height: 2em;
+  height: 1.5em;
   margin-right: 0.5em;
   will-change: filter;
 }
